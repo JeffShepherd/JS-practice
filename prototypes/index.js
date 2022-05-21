206f3723c41d1f8482706480a2e70e4906d24ccb
Jeff S <jeff.shep777@gmail.com>
2022-05-21 07:25:20 -0600

    Solve book prompts

--- prototypes/index.js
+++ prototypes/index.js
@@ -273,39 +273,41 @@ const classPrompts = {
 // DATASET: books from './datasets/books
 
 const bookPrompts = {
-  removeViolence() {
+  removeViolence(books) {
     // Your function should access the books data through a parameter (it is being passed as an argument in the test file)
     // return an array of all book titles that are not horror or true crime. Eg:
-
     //  ['1984', 'The Great Gatsby', 'Lord of the Flies', 'Harry Potter and the Sorcerer\'s Stone',
     //   'The Hitchhiker\'s Guide to the Galaxy', 'Flowers for Algernon', 'Slaughterhouse-Five',
     //   'The Handmaid\'s Tale', 'The Metamorphosis', 'Brave New World', 'Life of Pi',
     //   'The Curious Incident of the Dog in the Night - Time', 'The Bell Jar',
     //   'Catch-22', 'Treasure Island']
+  const result = books.reduce((bookList, book) => {
+    if(book.genre != 'Horror' && book.genre != 'True Crime') {
+      bookList.push(book.title)
+    }
+    return bookList
+  },[])
 
-
-    /* CODE GOES HERE */
-
-    // Annotation:
-    // Write your annotation here as a comment
-
+  return result
   },
-  getNewBooks() {
+
+  getNewBooks(books) {
     // return an array of objects containing all books that were
     // published in the 90's and 00's. Inlucde the title and the year Eg:
-
     // [{ title: 'Harry Potter and the Sorcerer\'s Stone', year: 1997 },
     //  { title: 'Life of Pi', year: 2001 },
     //  { title: 'The Curious Incident of the Dog in the Night-Time', year: 2003 }]
+    const result = books.reduce((bookList, book) => {
+      if(book.published > 1989) {
+        bookList.push({title:book.title, year:book.published})
+      }
+      return bookList
+    },[])
 
-    /* CODE GOES HERE */
-
-    // Annotation:
-    // Write your annotation here as a comment
+    return result
   }
 
 };
-
 
 // ---------------------------------------------------------------------------
 // ---------------------------------------------------------------------------
